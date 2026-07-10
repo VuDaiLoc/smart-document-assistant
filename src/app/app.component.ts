f716b9df36129f34dc63e94c6aa0e792d8423b4d
Vu Dai Loc <locvu2912@gmail.com>
2026-07-10 19:03:46 +0700

    fix : them file app.component.ts xoa nham

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,9 @@
-import { Component, signal } from '@angular/core';
+import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css',
 })
-export class App {
-  protected readonly title = signal('smart-document-assistant');
-}
+export class App {}
